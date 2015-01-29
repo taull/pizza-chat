a1398b5d0c1b7ae025b2774b1d39549402445b17
Erik Eggers <em.eggers@gmail.com>
2015-01-29 14:21:39 -0500

    added jquery and linked JS files correctly

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,12 @@
   'use strict';
 
 var pizzaUrl ="http://tiny-pizza-server.herokuapp.com/collections/greenville-chats";
+console.log("hello");
 
 $(document).ready(function() {
 
-  $.ajax(pizzaUrl).done(function(userData) {
-    console.log(userData);
+  $.ajax(pizzaUrl).done(function(data) {
+    console.log(data);
   });
 
 
