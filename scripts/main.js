7f386ab1730ee81803fbe35db4da560f38a88af1
Erik Eggers <em.eggers@gmail.com>
2015-01-29 16:56:26 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -28,11 +28,18 @@ $(document).ready(function() {
     url: "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats",
     type: "POST",
     data: {
-      message: "Time for bed",
+      message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       username: "Dad",
       createdAt: (new Date()).toString()
     }
   });
+
+
+
+
+
+
+
 
   });
 })();
